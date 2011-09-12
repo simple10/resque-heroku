d4bccafca6bd81f7b654c3a4d958ef966e450987
Joe Johnston <joe@connect.me>
2011-09-12 00:29:40 -0700

    fixed on_failure

--- lib/resque-heroku.rb
+++ lib/resque-heroku.rb
@@ -3,16 +3,10 @@ require 'resque-heroku/version'
 # extend Resque::Heroku in your job class
 module Resque::Heroku
   def after_perform_heroku
-    resque_heroku_disconnect!
+    ActiveRecord::Base.connection.disconnect!
   end
 
-  def failed_with_heroku!
-    resque_heroku_disconnect!
-  end
-
-  protected
-
-  def resque_heroku_disconnect!
+  def on_failure_heroku
     ActiveRecord::Base.connection.disconnect!
   end
 end
--- lib/resque-heroku/version.rb
+++ lib/resque-heroku/version.rb
@@ -1,5 +1,5 @@
 module Resque
   module Heroku
-    VERSION = "0.0.3"
+    VERSION = "0.0.4"
   end
 end
